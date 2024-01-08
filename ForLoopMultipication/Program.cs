1f5778f7ef02573ee92b74d70224cc8388cb2bf0
opilane <opilane@HP-CZC0359NP0>
2024-01-08 13:27:12 +0200

    Add project files.

---
+++ ForLoopMultipication/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ForLoopMultipication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Korrutustabel");
+
+            int j, i, n;
+
+            Console.WriteLine("Sisesta tabelisse number, millest soovid alustada");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Korrutustabel ühest kuni {0} ", n);
+            Console.Write("\n");
+
+            for (i = 1; i < 10; i++)
+            {
+                for (j = 1; j <= n; j++)
+                {
+                    if (j <= n - 1)
+                    {
+                        Console.Write("{0}x{1} = {2},", j, i, i * i);
+                    }
+                    else
+                    {
+                        Console.Write("{0}x{1} = {2} ", j, i, i * j);
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
